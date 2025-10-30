9d26278d6390bc6b309dd49905ad19fd2bdc2476
Zahh-id <zahidpulo53@gmail.com>
2025-10-30 19:25:30 +0700

    Update

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -10,8 +10,8 @@ return [
             ],
 
             'routes' => [
-                'api' => 'api/docs',
-                'docs' => 'api/docs.json',
+                'api' => 'api/docs.json',
+                'docs' => 'api/docs',
             ],
 
             'paths' => [
@@ -30,7 +30,7 @@ return [
 
     'defaults' => [
         'routes' => [
-            'docs' => 'api/docs.json',
+            'docs' => 'api/docs',
             'oauth2_callback' => 'api/oauth2-callback',
             'middleware' => [
                 'api' => [],
