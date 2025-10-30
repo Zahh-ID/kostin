c5a653a862668e19f72d41a73c56d9347cb446b0
Zahh-id <zahidpulo53@gmail.com>
2025-10-30 17:25:58 +0700

    Change view n routes n resouces

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Web\AuthController as WebAuthController;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,18 +15,3 @@ Route::post('/logout', [WebAuthController::class, 'logout'])->name('logout');
 Route::view('/dashboard', 'dashboard')
     ->middleware('auth')
     ->name('dashboard');
-
-Route::get('/api/docs.json', function () {
-    $docsPath = storage_path('api-docs');
-    $file = $docsPath.'/'.config('l5-swagger.documentations.v1.paths.docs_json', 'api-docs.json');
-
-    if (! file_exists($file)) {
-        Artisan::call('l5-swagger:generate');
-    }
-
-    abort_unless(file_exists($file), 404);
-
-    return response()->file($file, [
-        'Content-Type' => 'application/json',
-    ]);
-});
