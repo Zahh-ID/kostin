08e1fa4bc2ea8e3c2f3d75802677c9d4f7b3631b
Zahh-id <zahidpulo53@gmail.com>
2025-11-08 05:13:19 +0700

    add error handling on auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         @stack('styles')
+        @livewireStyles
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -65,6 +66,7 @@
             </footer>
         </div>
 
+        @livewireScripts
         @stack('scripts')
     </body>
 </html>
