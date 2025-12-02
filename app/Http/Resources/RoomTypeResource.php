1acf1532ae2c714a503f5dd4113a6e631cdb9596
Zahh-id <zahidpulo53@gmail.com>
2025-12-03 03:26:50 +0700

    done

--- app/Http/Resources/RoomTypeResource.php
+++ app/Http/Resources/RoomTypeResource.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @mixin \App\Models\RoomType
+ *
+ * @OA\Schema(
+ *     schema="RoomTypeResource",
+ *     type="object",
+ *     @OA\Property(property="id", type="integer"),
+ *     @OA\Property(property="property", ref="#/components/schemas/PropertyResource"),
+ *     @OA\Property(property="name", type="string"),
+ *     @OA\Property(property="base_price", type="number", format="float"),
+ *     @OA\Property(property="area_m2", type="number", format="float"),
+ *     @OA\Property(property="bathroom_type", type="string", enum={"inside","outside"}),
+ *     @OA\Property(property="deposit", type="number", format="float"),
+ *     @OA\Property(property="facilities_json", type="object"),
+ *     @OA\Property(property="rooms", type="array", @OA\Items(ref="#/components/schemas/RoomResource"))
+ * )
  */
 class RoomTypeResource extends JsonResource
 {
